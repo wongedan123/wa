b665c7b50163f4f9d00d205357597090905a5946
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-02-04 01:43:29 +0700

    Add files via upload

---
+++ ponsel.py
@@ -0,0 +1,228 @@
+#coded by Mr.w0n63d4n
+#jangn direcod k*ntol :v
+#hacker buta coding mau jadi apa ?
+#wkwkwkwk
+#just kidding
+import random,os,sys
+#untuk im3
+def im3():
+	nomor = random.choice([62855,62814,62815,62816,62856,62857,62858])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+#untuk xl
+def xl():
+	nomor = random.choice([62817,62818,62819,62859,62877,62878])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == 'y':
+		tanya()
+	elif back == 't':
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+		
+def telkomsel():
+	nomor = random.choice([62811,62812,62813,62822,62821,62823,62852,62851,62853])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+
+
+def three():
+	nomor = random.choice([62895,62896,62897,62898,62899])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+
+
+def smartfren():
+	nomor = random.choice([62881,62882,62883,62884,62885,62886,62887,62888,62889])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+
+def exis():
+	nomor = random.choice([62838,62831,62832,62833])
+	rdm = random.randint(0,100000009)
+	print "%d%d"%(nomor,rdm)
+	print "ingin mencari perawan lagi?"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+	
+def gram():
+	
+	print ""
+	print "ini adalah program anti jomblo :v"
+	print 'kalo dapat nomer bapak"/ibu"'
+	print "jangan salahkan program :v"
+	print "tapi teruslah mencari sampe dapat"
+	print "yang prawan :v"
+	print ""
+	back = raw_input("lanjut? (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+
+def saya():
+	print ""
+	print "======================================="
+	print "sebenarnya saya juga jomblo :v"
+	print "makanya saya buat program ini :v"
+	print "ya semoga anda bisa menikmati juga program saya :v"
+	print "======================================="
+	print ""
+	back = raw_input("mau balik ke menu apa ke mantan :v (y/t) :")
+	if back == "y":
+		tanya()
+	elif back == "t":
+		print "come back ya bos :D byee"
+	else:
+		print "belum input ketik lagi python2 ponsel.py"
+	
+def menu():
+	
+	print "\033[93m$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+	print "$                                    $"
+	print "$    coded by : Mr.w0n63d4n          $"
+	print "$    Instagram: @riezzky_maulana     $"
+	print "$    youtube  : wongedan official    $"
+	print "$    whatsapp : 085861185074         $"
+	print "$                                    $"
+	print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\033[0m"
+	print "\033[92m"
+	print "welcome program jomblo :v"
+	print "_________________________"
+	print ""
+	print "pilih type simcard"
+	print "=================="
+	print "[1]. im3"
+	print "[2]. telkomsel"
+	print "[3]. three"
+	print "[4]. smartfren"
+	print "[5]. exis"
+	print "[6]. tentang saya :v"
+	print "[7]. keluar"
+	print "[8]. tentang program"
+	print "[9]. link whatsapp"
+	print "[10].xl"
+	print ""
+	pilih = input("masukan pilihan :")
+
+	if pilih == 1:
+		print ""
+		print "hasil search number im3 :",im3()
+	elif pilih == 2:
+		print ""
+		print "hasil search number telkomsel :",telkomsel()
+	elif pilih == 3:
+		print ""
+		print "hasil search number three :",three()
+	elif pilih == 4:
+		print ""
+		print "hasil search number smartfren :",smartfren()
+	elif pilih == 5:
+		print ""
+		print "hasil search number exis :",exis()
+	elif pilih == 6:
+		saya()
+	elif pilih == 7:
+		os.system ('clear'),os.system ('exit'),
+		print "jangan lupa mampir lagi :D"
+	elif pilih == 8:
+		gram()
+	elif pilih == 9:
+		phone()
+	elif pilih == 10:
+		print ""
+		print "hasil search number xl :",xl()
+	else :
+		print "\033[91mpilihan yang anda maksud tidak tersedia !\033[0m"
+
+	
+def tanya():
+	back = raw_input("MENU|WA|KELUAR :")
+	if back == 'menu':
+		os.system ('clear'),
+		os.system ("toilet -f slant --gay jomblo"),
+		menu()
+	elif back == 'wa':
+		os.system ('clear'),
+		os.system ("toilet -f slant --gay jomblo"),
+		print "\033[92m",phone()
+	elif back == 'keluar':
+		print "come back ya bos :D byee"
+	else:
+		print "input salah ketik bos"
+		
+def phone():
+	os.system ('clear')
+	os.system ("toilet -f slant --gay jomblo")
+	print "\033[92m"
+	link = "https://api.whatsapp.com/send?phone="
+	text = "&text=testing%20:)"
+	sabyan = input("masukan no ponsel 0 ganti 62! :")
+	print "______________________________"
+	print "salin link dibawah ke chroome"
+	print "dan lihat keajaibannya :D"
+	print ""
+	print "======================="
+	print ("hajar bro ===> %s%d%s"%(link,sabyan,text))
+	print "======================="
+	print "______________________________"
+	tanya()
+#yopie rizqi maulana
+
+os.system ('clear')
+os.system ("toilet -f banner --gay jomblo")
+print "\033[92m"
+login = raw_input("masukan passwordnya bosku :")
+if login == "wongedan" :
+	menu()
+else:
+	print "\033[91mpassword salah bosku\033[0m"
+    
